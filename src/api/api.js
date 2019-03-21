0f1cc54c5f0c82d3bd6925f87a515cb6e12cb8c9
renhongqiang <renhongqiang@megvii.com>
2019-03-21 14:35:45 +0800

    init project

---
+++ src/api/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+let base = 'http://localhost:8085/security';
+
+/**用户管理相关接口**/
+let user = "user";
+export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+
+export const getUserList = params => { return axios.get(`${base}/${user}/getAll`, { params: params }); };
+
+export const getUserListPage = params => { return axios.post(`${base}/${user}/list`, params).then(res => res.data); };
+
+export const removeUser = params => { return axios.get(`${base}/${user}/remove`, { params: params }); };
+
+export const batchRemoveUser = params => { return axios.get(`${base}/${user}/batchremove`, { params: params }); };
+
+export const updateUser = params => { return axios.post(`${base}/${user}/update`,  params ).then(res => res.data)};
+
+export const addUser = params => { return axios.post(`${base}/${user}/add`, params).then(res => res.data)};
+
+
+/**角色管理相关接口**/
+let role = "role";
+export const getRoleList = params => { return axios.get(`${base}/${role}/getAll`, { params: params }); };
+
+export const getRoleListPage = params => { return axios.post(`${base}/${role}/list`, params).then(res => res.data); };
+
+export const removeRole = params => { return axios.get(`${base}/${role}/remove`, { params: params }); };
+
+export const updateRole = params => { return axios.post(`${base}/${role}/update`,  params ).then(res => res.data)};
+
+export const addRole = params => { return axios.post(`${base}/${role}/add`, params).then(res => res.data)};
+
+
---
+++ src/views/user/Role.vue
@@ -0,0 +1,260 @@
+<template>
+	<section>
+		<!--工具条-->
+		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+			<el-form :inline="true" :model="filters">
+				<el-form-item style="width: 140px;">
+					<el-input v-model="filters.name" placeholder="角色名"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" v-on:click="getRoles">查询</el-button>
+				</el-form-item>
+				<el-form-item style="float: right;">
+					<el-button type="primary" @click="handleAdd">新增</el-button>
+				</el-form-item>
+			</el-form>
+		</el-col>
+
+		<!--列表-->
+		<el-table :data="roles" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
+			<el-table-column type="selection" width="55">
+			</el-table-column>
+			<el-table-column type="index" width="60">
+			</el-table-column>
+			<el-table-column prop="name" label="角色名称" width="120" sortable>
+			</el-table-column>
+			<el-table-column prop="key" label="角色key" width="130" sortable>
+			</el-table-column>
+			<el-table-column prop="desc" label="备注" min-width="180" sortable>
+			</el-table-column>
+			<el-table-column label="操作" width="150">
+				<template scope="scope">
+					<el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+					<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+
+		<!--工具条-->
+		<el-col :span="24" class="toolbar">
+			<el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="pageSize" :total="total" style="float:right;">
+			</el-pagination>
+		</el-col>
+
+		<!--编辑界面-->
+		<el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
+			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
+				<el-form-item label="id" prop="id" v-show="false">
+					<el-input v-model="editForm.id" ></el-input>
+				</el-form-item>
+				<el-form-item label="角色名称" prop="name">
+					<el-input v-model="editForm.name" auto-complete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="角色key">
+					<el-input-number v-model="editForm.key" :min="0" :max="200" :step="1"></el-input-number>
+				</el-form-item>
+				<el-form-item label="备注">
+					<el-input type="textarea" v-model="editForm.desc"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click.native="editFormVisible = false">取消</el-button>
+				<el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
+			</div>
+		</el-dialog>
+
+		<!--新增界面-->
+		<el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
+			<el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+				<el-form-item label="角色名称" prop="name">
+					<el-input v-model="addForm.name" auto-complete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="角色key" prop="mail">
+					<el-input v-model="addForm.key" auto-complete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="备注">
+					<el-input type="textarea" v-model="addForm.desc"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click.native="addFormVisible = false">取消</el-button>
+				<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+			</div>
+		</el-dialog>
+	</section>
+</template>
+
+<script>
+	import util from '../../common/js/util'
+	//import NProgress from 'nprogress'
+	import { getRoleListPage, removeRole, updateRole, addRole } from '../../api/api';
+
+	export default {
+		data() {
+			return {
+				filters: {
+					name: ''
+				},
+				roles: [],
+				total: 0,
+				pageSize:5,
+				pageNum: 1,
+				listLoading: false,
+				sels: [],//列表选中列
+
+				editFormVisible: false,//编辑界面是否显示
+				editLoading: false,
+				editFormRules: {
+					name: [
+						{ required: true, message: '请输入角色名称', trigger: 'blur' }
+					]
+				},
+				//编辑界面数据
+				editForm: {
+					id: 0,
+					name: '',
+					key: '',
+					desc: ''
+				},
+
+				addFormVisible: false,//新增界面是否显示
+				addLoading: false,
+				addFormRules: {
+					name: [
+						{ required: true, message: '请输入角色名称', trigger: 'blur' }
+					]
+				},
+				//新增界面数据
+				addForm: {
+					id: 0,
+					name: '',
+					key: '',
+					desc: ''
+				}
+
+			}
+		},
+		methods: {
+			handleCurrentChange(val) {
+				this.pageNum = val;
+				this.getRoles();
+			},
+			//获取用户列表
+			getRoles() {
+				let para = {
+					pageNum: this.pageNum,
+					pageSize: this.pageSize,
+					name: this.filters.name
+				};
+				this.listLoading = true;
+				//NProgress.start();
+				getRoleListPage(para).then((res) => {
+					this.total = res.data.totalCount;
+					this.roles = res.data.list;
+					this.listLoading = false;
+					//NProgress.done();
+				});
+			},
+			//删除
+			handleDel: function (index, row) {
+				this.$confirm('确认删除该记录吗?', '提示', {
+					type: 'warning'
+				}).then(() => {
+					this.listLoading = true;
+					//NProgress.start();
+					let para = { id: row.id };
+					removeRole(para).then((res) => {
+						this.listLoading = false;
+						//NProgress.done();
+						this.$message({
+							message: '删除成功',
+							type: 'success'
+						});
+						this.getRoles();
+					});
+				}).catch(() => {
+
+				});
+			},
+			//显示编辑界面
+			handleEdit: function (index, row) {
+				this.editFormVisible = true;
+				this.editForm = Object.assign({}, row);
+			},
+			//显示新增界面
+			handleAdd: function () {
+				this.addFormVisible = true;
+				this.addForm = {
+					name: '',
+					mail:'',
+					sex: -1,
+					age: 0,
+					birth: '',
+					address: ''
+				};
+			},
+			//编辑
+			editSubmit: function () {
+				this.$refs.editForm.validate((valid) => {
+					if (valid) {
+						this.$confirm('确认提交吗？', '提示', {}).then(() => {
+							this.editLoading = true;
+							//NProgress.start();
+							let para = Object.assign({}, this.editForm);
+							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
+							updateRole(para).then((res) => {
+								this.editLoading = false;
+								//NProgress.done();
+								this.$message({
+									message: '提交成功',
+									type: 'success'
+								});
+								this.$refs['editForm'].resetFields();
+								this.editFormVisible = false;
+								this.getRoles();
+							});
+						});
+					}
+				});
+			},
+			//新增
+			addSubmit: function () {
+				this.$refs.addForm.validate((valid) => {
+					if (valid) {
+						this.$confirm('确认提交吗？', '提示', {}).then(() => {
+							this.addLoading = true;
+							//NProgress.start();
+							let para = Object.assign({}, this.addForm);
+							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
+							addRole(para).then((res) => {
+								this.addLoading = false;
+								//NProgress.done();
+								this.$message({
+									message: '提交成功',
+									type: 'success'
+								});
+								this.$refs['addForm'].resetFields();
+								this.addFormVisible = false;
+								this.getRoles();
+							});
+						});
+					}
+				});
+			},
+			selsChange: function (sels) {
+				this.sels = sels;
+			},
+		},
+		mounted() {
+			this.getUsers();
+		}
+	}
+
+</script>
+
+<style scoped>
+	.el-input, .el-input__inner {
+		width: 75%;
+		display: inline-block;
+	}
+</style>
